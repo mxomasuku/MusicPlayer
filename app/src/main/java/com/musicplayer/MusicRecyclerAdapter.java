1d2302681a3e8aacf33bf587f090fc2bb9021e61
Mxolisi Masuku <64246766+mxomasuku@users.noreply.github.com>
2020-09-29 06:22:30 +0200

    First Commit on experimental branch: Adapter setting up.

--- app/src/main/java/com/musicplayer/MusicRecyclerAdapter.java
+++ app/src/main/java/com/musicplayer/MusicRecyclerAdapter.java
@@ -1,4 +1,35 @@
 package com.musicplayer;
 
-public class MusicRecyclerAdapter  {
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MusicRecyclerAdapter extends RecyclerView.Adapter<MusicRecyclerAdapter.ViewHolder>  {
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		return null;
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+	}
+
+	@Override
+	public int getItemCount() {
+		return 0;
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder{
+
+		public ViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
+	}
+
 }
