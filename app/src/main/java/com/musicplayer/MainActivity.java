8de39dd4fec92993d163780d016e8306f6fe0c4f
Mxolisi Masuku <64246766+mxomasuku@users.noreply.github.com>
2020-09-29 02:00:38 +0200

    Added a recyclerView dependancy to Gradle. Created a recycler view in the layout and attached in the MainActivity.java

--- app/src/main/java/com/musicplayer/MainActivity.java
+++ app/src/main/java/com/musicplayer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		initializeDisplayContent();
+	}
+
+	private void initializeDisplayContent() {
+		final RecyclerView musicRecyclerView = findViewById(R.id.music_recycler_list);
+		final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+		musicRecyclerView.setLayoutManager(layoutManager);
 	}
 }
